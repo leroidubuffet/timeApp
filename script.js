4398754248c3d6f053d407aa63f9ac91e82d2b11
Yago Bolívar <mr.bijou@gmail.com>
2023-05-15 15:27:36 +0200

    control and timer overlapping

--- script.js
+++ script.js
@@ -1,10 +1,48 @@
-let timer = document.getElementById("timer");
+let timerElement = document.getElementById("timer");
+let controlTimerElement = document.getElementById("control-timer");
+let minutesElement = document.getElementById("minutes");
+let secondsElement = document.getElementById("seconds");
 
-function updateTime() {
-  let date = new Date();
-  let minutes = date.getMinutes().toString().padStart(2, '0');
-  let seconds = date.getSeconds().toString().padStart(2, '0');
-  timer.textContent = `${minutes}:${seconds}`;
+let timer = null;
+let countdown = 0;
+
+timerElement.addEventListener('click', function() {
+  // Show the control timer
+  controlTimerElement.style.display = 'flex';
+});
+
+function updateCountdown() {
+  let minutes = Math.floor(countdown / 60).toString().padStart(2, '0');
+  let seconds = (countdown % 60).toString().padStart(2, '0');
+  timerElement.textContent = `${minutes}:${seconds}`;
+
+  if (countdown > 0) {
+    countdown--;
+  } else {
+    clearInterval(timer);
+  }
 }
 
-setInterval(updateTime, 1000);
+minutesElement.addEventListener('change', function() {
+  countdown = minutesElement.value * 60 + secondsElement.value * 1; // Multiply minutes by 60 to convert to seconds
+  updateCountdown();
+});
+
+secondsElement.addEventListener('change', function() {
+  countdown = minutesElement.value * 60 + secondsElement.value * 1; // Multiply minutes by 60 to convert to seconds
+  updateCountdown();
+});
+
+// Start the countdown
+timer = setInterval(updateCountdown, 1000);
+
+// Hide the control timer and start the countdown when the inputs lose focus
+minutesElement.addEventListener('blur', function() {
+  controlTimerElement.style.display = 'none';
+  timer = setInterval(updateCountdown, 1000);
+});
+
+secondsElement.addEventListener('blur', function() {
+  controlTimerElement.style.display = 'none';
+  timer = setInterval(updateCountdown, 1000);
+});
