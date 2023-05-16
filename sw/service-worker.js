d3030b3d9d204f154c0c6bea2a4d67dc930e02df
Yago Bolívar <mr.bijou@gmail.com>
2023-05-16 15:02:13 +0200

    service worker routes updated

--- sw/service-worker.js
+++ sw/service-worker.js
@@ -1,40 +1,40 @@
 const CACHE_NAME = 'countdown-timer-pwa-v1';
 const FILES_TO_CACHE = [
-  '/',
-  './public/index.html',
-  './public/manifest.json',
-  './src/styles.css',
-  './src/scripts/app.js',
-  './public/imgs/icon-192.png',
-  './public/imgs/icon-512.png'
+	'../',
+	'../public/index.html',
+	'../public/manifest.json',
+	'../src/styles.css',
+	'../src/scripts/app.js',
+	'../public/imgs/icon-192.png',
+	'../public/imgs/icon-512.png'
 ];
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
-  );
+	event.waitUntil(
+		caches.open(CACHE_NAME).then(cache => {
+			return cache.addAll(FILES_TO_CACHE);
+		})
+	);
 });
 
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keyList => {
-      return Promise.all(
-        keyList.map(key => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      );
-    })
-  );
+	event.waitUntil(
+		caches.keys().then(keyList => {
+			return Promise.all(
+				keyList.map(key => {
+					if (key !== CACHE_NAME) {
+						return caches.delete(key);
+					}
+				})
+			);
+		})
+	);
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+	event.respondWith(
+		caches.match(event.request).then(response => {
+			return response || fetch(event.request);
+		})
+	);
 });
